19c1dbb3d2cd54c7c8d8353443150c108086441f
emanuel-teodoro <168236917+emanuel-teodoro@users.noreply.github.com>
2025-04-04 14:41:28 -0300

    Add files via upload

---
+++ ESTRUTURA REPETIÇÃO/ESTRUTURA REPETIÇÃO/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N1, N2;
+        string resposta;
+        //UTILIZAÇÃO DE ESTRUTURAS CONDICIONAIS COMO DO/WHILE E IF/ELSE
+        do {//DO PARA RODAR O CODIGO TODO EM LOOP ATE O USUARIO DECIDIR ENCERRAR
+            
+            Console.WriteLine("Informe o 1 valor");
+            N1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o 2 valor");
+            N2 = int.Parse(Console.ReadLine());
+               
+            //CORPO DO CODIGO UTILIZANDO IF/ELSE
+                if (N1 > N2)
+                {
+                    Console.WriteLine("O maior valor é: " + N1);
+                }
+                else if (N2 > N1)
+                {
+                    Console.WriteLine("O maior valor é: " + N2);
+                }
+                else
+                {
+                    Console.WriteLine("Ambos os valores são iguais");
+                }
+
+                Console.WriteLine("Deseja realizar outra operação SIM/NÃO");
+                resposta = Console.ReadLine().ToLower();
+                
+        } while (resposta == "sim");//CONDIÇÃO QUE MANTEM O CODIGO RODANDO EM LOOP ATE O USUARIO PARAR
+
+        Console.WriteLine("Programa encerrado");
+    }
+}
